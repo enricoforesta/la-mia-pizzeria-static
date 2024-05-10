3c7d64e70f8ca97adb7a417fa70973d338c583d1
enricoforesta <enrico.foresta97@gmail.com>
2024-05-10 16:15:51 +0200

    feat: fine esercizio

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,26 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? PizzaImg { get; set; }
+
+        public decimal? Price { get; set; }
+
+
+        public Pizza() { }
+
+        public Pizza(string name, string description, string pizzaImg, decimal price) 
+        {
+            this.Name = name;
+            this.Description = description;
+            this.PizzaImg = pizzaImg;
+            this.Price = price;
+        }
+    }
+
+   
+}
